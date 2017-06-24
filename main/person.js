2707d92add7b75e8f6603b681ea9608d4e822a4c
brobird <netassadove@gmail.com>
2017-06-24 17:05:19 +0800

    initial

--- main/person.js
+++ main/person.js
@@ -1,2 +1,11 @@
 // Write your code here
 module.exports = Person
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+Person.prototype.introduce = function() {
+  var txt = "My name is " + this.name + ". I am " + age + "years old.";
+  return txt;
+}
--- main/student.js
+++ main/student.js
@@ -1,3 +1,17 @@
 let Person = require('./person')
 // Write your code here
 module.exports = Student
+
+function Student(name, age, kclass) {
+  Person.call(this, name, age);
+  this.kclass = kclass;
+}
+
+Student.prototype = new Person();
+
+Student.prototype.introduce = function () {
+  var txt = "My name is " + this.name + ".";
+  txt += "I am " + this.age + "years old.";
+  txt += "I am a student. I am at Class " + this.kclass + ".";
+  return txt;
+}
